0358e62b84ba73b3e886146de7047a47bfe617cc
Swashata Ghosh <swashata@wpquark.com>
2018-09-24 16:56:23 +0530

    refactor: use better looping for selecting plugins
    Instead of repetative if(cond) arr.push(..), write it in a better
    way to make future me, love current me. 🤟❤️

--- packages/babel-preset-base/src/preset.js
+++ packages/babel-preset-base/src/preset.js
@@ -1,11 +1,12 @@
 export default (opts = {}) => {
 	// Extract this preset specific options and pass the rest to @babel/preset-env
 	const {
-		noDynamicImport = false,
-		noImportMeta = false,
-		noClassProperties = false,
-		noJsonStrings = false,
+		// noDynamicImport = false,
+		// noImportMeta = false,
+		// noClassProperties = false,
+		// noJsonStrings = false,
 		presetEnv = {},
+		...noPlugins
 	} = opts || {};
 
 	// Create the presets
@@ -13,22 +14,21 @@ export default (opts = {}) => {
 
 	// Create the plugins
 	const plugins = [];
-	// Add them, only if user hasn't explicitly disabled it
-	if (!noDynamicImport) {
-		plugins.push('@babel/plugin-syntax-dynamic-import');
-	}
-	if (!noImportMeta) {
-		plugins.push('@babel/plugin-syntax-import-meta');
-	}
-	if (!noClassProperties) {
-		plugins.push([
+	const wannabePlugins = {
+		noDynamicImport: '@babel/plugin-syntax-dynamic-import',
+		noImportMeta: '@babel/plugin-syntax-import-meta',
+		noClassProperties: [
 			'@babel/plugin-proposal-class-properties',
 			{ loose: false },
-		]);
-	}
-	if (!noJsonStrings) {
-		plugins.push('@babel/plugin-proposal-json-strings');
-	}
+		],
+		noJsonStrings: '@babel/plugin-proposal-json-strings',
+	};
+	// Add them, only if user hasn't explicitly disabled it
+	Object.keys(wannabePlugins).forEach(pKey => {
+		if (noPlugins[pKey] !== true) {
+			plugins.push(wannabePlugins[pKey]);
+		}
+	});
 
 	// Return the preset and some of stage-3 plugins
 	// We will remove them, once it becomes stage-4, i.e included in preset-env
