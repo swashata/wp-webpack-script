c67919fde1b1ba63224b3fe7cc7b5865632d996a
Swashata Ghosh <swashata@wpquark.com>
2018-10-05 20:54:35 +0530

    refactor: fix some linting issues

--- packages/babel-preset-base/jest.config.js
+++ packages/babel-preset-base/jest.config.js
@@ -1,8 +1,8 @@
 const base = require('../../jest.base');
-const package = require('./package.json');
+const pkg = require('./package.json');
 
 module.exports = {
 	...base,
-	name: package.name,
-	displayName: package.name,
+	name: pkg.name,
+	displayName: pkg.name,
 };
