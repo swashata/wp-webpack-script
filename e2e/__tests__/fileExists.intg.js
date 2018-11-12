ce3ac1865e231b3a9407a9cabf60506725977465
Swashata Ghosh <swashata@wpquark.com>
2018-11-12 20:13:41 +0530

    ci: update e2e test
    Test out the pack feature

--- e2e/__tests__/fileExists.intg.js
+++ e2e/__tests__/fileExists.intg.js
@@ -14,7 +14,12 @@ function fileExists(filepath) {
 
 describe('after build', () => {
 	test('all javascript app got compiled', () => {
-		const jsApps = ['app/runtime.js', 'app/main.js', 'app/main.css'];
+		const jsApps = [
+			'app/runtime.js',
+			'app/main.js',
+			'app/main.css',
+			'app/manifest.json',
+		];
 		jsApps.forEach(file => {
 			expect(
 				fileExists(path.resolve(__dirname, '../dist/', file))
@@ -22,11 +27,45 @@ describe('after build', () => {
 		});
 	});
 	test('all typescript app got compiled', () => {
-		const tsApps = ['tsapp/runtime.js', 'tsapp/main.js', 'tsapp/main.css'];
+		const tsApps = [
+			'tsapp/runtime.js',
+			'tsapp/main.js',
+			'tsapp/main.css',
+			'tsapp/manifest.json',
+		];
 		tsApps.forEach(file => {
 			expect(
 				fileExists(path.resolve(__dirname, '../dist/', file))
 			).toBeTruthy();
 		});
+	});
+});
+
+describe('after pack', () => {
+	test('all files got copied', () => {
+		const zipFiles = [
+			'index.html',
+			'README.md',
+			'dist/app/runtime.js',
+			'dist/app/main.js',
+			'dist/app/main.css',
+			'dist/app/manifest.json',
+			'dist/tsapp/runtime.js',
+			'dist/tsapp/main.js',
+			'dist/tsapp/main.css',
+			'dist/tsapp/manifest.json',
+		];
+		zipFiles.forEach(file => {
+			expect(
+				fileExists(
+					path.resolve(__dirname, '../package/e2e-plug/', file)
+				)
+			).toBeTruthy();
+		});
+	});
+	test('zip file got created', () => {
+		expect(
+			fileExists(path.resolve(__dirname, '../package/e2e-plug.zip'))
+		).toBeTruthy();
 	});
 });
