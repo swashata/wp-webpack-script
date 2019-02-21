d3e8abdc3e9d6cc46dc81f1ff60bb41fd873b29c
Swashata Ghosh <swashata@wpquark.com>
2019-02-21 15:10:14 +0530

    feat: properly add barrel for nodejs API
    Keep it undocumented for now, till I personally use and test them.

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,16 +1,47 @@
-import { CreateWebpackConfig } from './config/CreateWebpackConfig';
-import { projectConfigDefault } from './config/project.config.default';
-import { serverConfigDefault } from './config/server.config.default';
-import { WebpackConfigHelper } from './config/WebpackConfigHelper';
-import { Build } from './scripts/Build';
-import { Server } from './scripts/Server';
-
 // Expose our Nodejs APIs for applications not using as CLI
+// and for using with tools like jest
+
+// Expose all needed config functions, classes and interfaces
 export {
-	CreateWebpackConfig,
+	typelang,
+	getBabelConfig,
+	getBabelPresetOptions,
+	getDefaultBabelPresetOptions,
+} from './config/babelConfig';
+
+export { CreateWebpackConfig, WpackConfig } from './config/CreateWebpackConfig';
+
+export {
 	WebpackConfigHelper,
+	WebpackConfigHelperConfig,
+} from './config/WebpackConfigHelper';
+
+export {
+	getProjectAndServerConfig,
+	getProjectConfig,
+	getServerConfig,
+	validateProjectConfig,
+	validateServerConfig,
+} from './config/getProjectAndServerConfig';
+
+export {
+	BannerConfig,
+	EntryConfig,
+	FileConfig,
+	ProjectConfig,
 	projectConfigDefault,
+	webpackLoaderOptionsOverride,
+	webpackOptionsOverrideFunction,
+} from './config/project.config.default';
+
+export {
+	ServerConfig,
 	serverConfigDefault,
-	Build,
-	Server,
-};
+} from './config/server.config.default';
+
+// Expose all needed script classes
+export { Build } from './scripts/Build';
+
+export { Server } from './scripts/Server';
+
+export { Bootstrap, ProjectDependencies } from './scripts/Bootstrap';
